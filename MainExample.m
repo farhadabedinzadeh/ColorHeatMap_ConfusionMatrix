8a834c8ef2cb9abdf37fb2b75f9a4b3f92297987
Farhad Abedinzadeh Torghabeh <96732467+farhadabedinzadeh@users.noreply.github.com>
2022-11-14 09:26:01 +0330

    Add files via upload

---
+++ MainExample.m
@@ -0,0 +1,12 @@
+clc;clear;close
+%%  Input Arguments
+%   Actual_Labels: Vector of Actual(Target)Class Labels
+%   Predicted_Labels: Vector of Predicted(Output)Class Labels
+%   HStyle : HeatMap Style
+%   -----------------------------------------------------------------------------
+%   farhaad.abedinzade@gmail.com | Master of Biomedical Engineering |November2022
+%%  -----------------------------------------------------------------------------
+
+load InOut
+
+[ConfMat,Accuracy] = CmapConfusionMatrix(actual_labels,predicted_labels);
